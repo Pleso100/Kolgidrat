a465efaf0a929504e7632d51f51ab77abc0755ce
Andrii <riabininmv@gmail.com>
2023-08-24 16:51:21 +0200

    2

--- main.py
+++ main.py
@@ -9,19 +9,21 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode, ReplyKeyboardRemove
 
-# Зчитування конфігурацій
+# Імпорт необхідних модулів
+
+# Зчитування конфігурацій з файлу config.ini
 config = ConfigParser()
 config.read('config.ini')
 bot_token = config.get('bot', 'token')
 
-# Зчитування пароля з config.ini
+# Зчитування пароля адміністратора з config.ini
 admin_password = config.get('admin', 'password', fallback=None)
 
-# Підключення до бази даних
+# Підключення до бази даних SQLite
 conn = sqlite3.connect('products.db')
 cursor = conn.cursor()
 
-# Створення таблиці, якщо її ще немає
+# Створення таблиці у базі даних, якщо її ще не існує
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,25 +34,27 @@ cursor.execute('''
 ''')
 conn.commit()
 
-# Налаштування логування
+# Налаштування системи логування
 logging.basicConfig(level=logging.INFO)
 
-# Ініціалізація MemoryStorage
+# Ініціалізація MemoryStorage для зберігання станів
 storage = MemoryStorage()
 
-# Ініціалізація бота та диспетчера з використанням MemoryStorage
+# Ініціалізація об'єкту бота та диспетчера з використанням MemoryStorage
 bot = Bot(token=bot_token)
 dp = Dispatcher(bot, storage=storage)
 dp.middleware.setup(LoggingMiddleware())
 
+# Визначення станів для обробки різних кроків взаємодії з користувачем
 class Form(StatesGroup):
-    name = State()
-    carbs = State()
-    he = State()
+    name = State()  # Стан для отримання назви продукту
+    carbs = State()  # Стан для отримання вмісту вуглеводів
+    he = State()  # Стан для отримання кількості хлібних одиниць
 
 class Form2(StatesGroup):
-    remove_name = State()
+    remove_name = State()  # Стан для видалення назви продукту
 
+# Функція для пошуку продуктів за запитом і відправки результатів користувачу
 async def search_and_reply(query: str, message: types.Message):
     if len(query) >= 2:
         search_input = query.lower()  # Перетворення в нижній регістр
@@ -62,24 +66,27 @@ async def search_and_reply(query: str, message: types.Message):
             reply_text = "Результати пошуку:\n"
             for result in results:
                 name, carbs, he = result
-                reply_text += f"Назва: {name}\nКолгідрати (вуглеводні) на 100 грам: {carbs}\nХлібні одиниці (приблизно): {he}\n\n"
+                reply_text += f"Назва: {name}\nКолгідрати (вуглеводи) на 100 грам: {carbs}\nХлібні одиниці (приблизно): {he}\n\n"
             await message.reply(reply_text)
         else:
             await message.reply("Продукти за Вашим запитом не знайдені.")
     else:
         await message.reply("Введіть не менше ніж дві букви для пошуку.")
 
+# Функція для видалення продукту з бази даних за назвою
 async def remove_product_from_database(product_name: str):
     query = "DELETE FROM products WHERE LOWER(name) = ?"
     cursor.execute(query, (product_name,))
     conn.commit()
 
+# Обробник для кнопки "Видалити продукт"
 @dp.callback_query_handler(lambda c: c.data == "remove_product")
 async def handle_remove_product(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, "Режим видалення продуктів. Введіть назву продукту для видалення:")
     await Form2.remove_name.set()
 
+# Обробник для отримання назви продукту, який потрібно видалити
 @dp.message_handler(state=Form2.remove_name)
 async def process_remove_name(message: types.Message, state: FSMContext):
     product_name_to_remove = message.text.strip().lower()  # Перетворення в нижній регістр та видалення зайвих пробілів
@@ -87,7 +94,7 @@ async def process_remove_name(message: types.Message, state: FSMContext):
     # Виклик функції для видалення продукту за його назвою
     await remove_product_from_database(product_name_to_remove)
 
-    # Remove keyboard
+    # Видалення клавіатури
     markup = ReplyKeyboardRemove()
 
     await bot.send_message(
@@ -101,12 +108,14 @@ async def process_remove_name(message: types.Message, state: FSMContext):
         parse_mode=ParseMode.MARKDOWN
     )
 
-    await state.finish()
+    await state.finish()  # Завершення стану
 
+# Обробник для команди "/start"
 @dp.message_handler(commands=['start'])
 async def handle_start(message: types.Message):
     await message.reply("Вітаю! Введіть не менше ніж дві букви для пошуку продуктів.")
 
+# Обробник для взаємодії з користувачем
 @dp.message_handler(lambda message: True)
 async def handle_message(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
@@ -124,6 +133,7 @@ async def handle_message(message: types.Message, state: FSMContext):
         else:
             await search_and_reply(message.text, message)
 
+# Обробник для кнопки "Адміністрування"
 @dp.callback_query_handler(lambda c: c.data == "admin_access")
 async def handle_admin_button(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
@@ -136,12 +146,14 @@ async def handle_admin_button(callback_query: types.CallbackQuery, state: FSMCon
     async with state.proxy() as data:
         data['admin_access'] = True
 
+# Обробник для кнопки "Додати продукт"
 @dp.callback_query_handler(lambda c: c.data == "add_product")
 async def handle_add_product(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, "Режим додавання продуктів. Введіть назву продукту:")
     await Form.name.set()
 
+# Обробник для отримання назви нового продукту
 @dp.message_handler(state=Form.name)
 async def process_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
@@ -150,6 +162,7 @@ async def process_name(message: types.Message, state: FSMContext):
     await Form.next()
     await message.reply("Введіть кількість вуглеводів на 100 грам у продукті:")
 
+# Обробник для отримання кількості вуглеводів нового продукту
 @dp.message_handler(lambda message: message.text.replace('.', '', 1).isdigit(), state=Form.carbs)
 async def process_carbs(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
@@ -158,6 +171,7 @@ async def process_carbs(message: types.Message, state: FSMContext):
     await Form.next()
     await message.reply("Введіть кількість хлібних одиниць у продукті:")
 
+# Обробник для отримання кількості хлібних одиниць нового продукту
 @dp.message_handler(lambda message: message.text.replace('.', '', 1).isdigit(), state=Form.he)
 async def process_he(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
@@ -165,7 +179,7 @@ async def process_he(message: types.Message, state: FSMContext):
 
     await state.update_data(he=float(message.text))
 
-    # Remove keyboard
+    # Видалення клавіатури
     markup = ReplyKeyboardRemove()
 
     await bot.send_message(
@@ -188,13 +202,15 @@ async def process_he(message: types.Message, state: FSMContext):
     )
     conn.commit()
 
-    await state.finish()
+    await state.finish()  # Завершення стану
 
+# Обробник для команди "/start"
 @dp.message_handler(lambda message: message.text == "/start")
 async def handle_start_command(message: types.Message, state: FSMContext):
     await state.finish()
     await handle_start(message)
 
+# Запуск бота через виконавця
 if __name__ == '__main__':
     from aiogram import executor
 
